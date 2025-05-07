67dd3f90e90a06e732b6f0ae9c7f089dbc9927c4
Maxence DUVAL <maxence.duval@zetaly.io>
2025-05-07 15:26:47 +0200

    Remove useless code

--- src/components.ts
+++ src/components.ts
@@ -1,44 +1,9 @@
-import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-import Message from 'primevue/message'
-import Password from 'primevue/password'
-import Divider from 'primevue/divider'
-import Popover from 'primevue/popover'
-import Select from 'primevue/select'
-import Column from 'primevue/column'
-import MultiSelect from 'primevue/multiselect'
-import FileUpload from 'primevue/fileupload'
-import Textarea from 'primevue/textarea'
-import InputNumber from 'primevue/inputnumber'
+// PrimeVue components
 import DatePicker from 'primevue/datepicker'
-import Tag from 'primevue/tag'
-import Tabs from 'primevue/tabs'
-import TabList from 'primevue/tablist'
-import Tab from 'primevue/tab'
-import TabPanels from 'primevue/tabpanels'
-import TabPanel from 'primevue/tabpanel'
-
+// Custom components
 import DateRangePicker from './components/DateRangePicker.vue'
 
 export default {
-  Button,
-  InputText,
-  Message,
-  Password,
-  Divider,
-  Popover,
-  Select,
-  Column,
-  MultiSelect,
-  FileUpload,
-  Textarea,
-  InputNumber,
   DatePicker,
-  Tag,
-  Tabs,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
   DateRangePicker,
 }
